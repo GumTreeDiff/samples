b23f2cfa68aee5a51e35d60b4c2b63ed4f60bbe0
KOLANICH <kolan_n@mail.ru>
2016-09-04 14:20:49 +0000

    Added some C# files (made from c)

---
+++ csharp/patch_v1.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Foo{
+    public class Bar{
+        public static int Main() {
+            int a = 0;
+            int b = 0;
+            if (a < b)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
