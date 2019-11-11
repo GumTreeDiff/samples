39985be027cdfe8d70baaf9c5e2368e366e5ac21
Alex Nguyen <itsAlexNguyen@users.noreply.github.com>
2019-11-10 21:30:44 -0500

    Update simplediff.sh

--- .travis/simplediff.sh
+++ .travis/simplediff.sh
@@ -1,3 +1,3 @@
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
 -d "{\"body\": \"Testing comments\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
