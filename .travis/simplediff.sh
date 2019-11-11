c726433a42765b7e0811c4c0f98460985e035e64
Alex Nguyen <itsAlexNguyen@users.noreply.github.com>
2019-11-10 21:42:09 -0500

    Update simplediff.sh

--- .travis/simplediff.sh
+++ .travis/simplediff.sh
@@ -1,3 +1,3 @@
 curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
--d "{\"body\": \"I generated a SimpleDiff here -> https://tranquil-earth-41184.herokuapp.com/diff?branch=${TRAVIS_BRANCH}\"}" \
+-d "{\"body\": \"I generated a SimpleDiff here -> https://tranquil-earth-41184.herokuapp.com/diff?branch=${TRAVIS_PULL_REQUEST_BRANCH}\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
