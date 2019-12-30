24c9a4bee826a28637614fe130c24e6ca50dee65
Phillip Pavlich <pavlichphillip@yahoo.ca>
2019-12-30 18:05:42 -0500

    alter example v2
    changing 2nd file

--- Example_v1.java
+++ Example_v1.java
@@ -1,9 +1,14 @@
 public class B {
     private static int first;
     private static int second = 5;
+    private static int third = 52;
 
     public B(){
         B.first = 21;
+    }
+    
+    public int add(){
+        return B.first + B.second;
     }
     
     public int mult(){
